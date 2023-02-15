818c10f893595a95179e582f160b1c96cf5c4b16
ZIAUL HAQUE <56592240+open4profit@users.noreply.github.com>
2023-02-16 02:32:04 +0400

    Update long.js

--- futuros-bitget/long.js
+++ futuros-bitget/long.js
@@ -3,10 +3,11 @@ const client = require('./services/client')
 
 const PAIR1 = process.argv[2] || 'BTC'
 const PAIR2 = process.argv[3] || 'USDT'
-const AMOUNT = process.argv[4] || 15
-const LEVERAGE = process.argv[5] || 5
+const AMOUNT = process.argv[4] || 30
+const LEVERAGE = process.argv[5] || 10
 const TAKE_PROFIT_PERCENT = process.argv[6] || 0.3
 const STOP_LOSS_PERCENT = process.argv[7] || 1
+//const EXE_PRICE = process.argv[8] || 23300
 
 const replaceMultipler = (number, decimals, multipler) => {
     number = number.toString()
@@ -38,13 +39,13 @@ async function placeLongPosition() {
         symbol: `${market}_UMCBL`
     }
 
-    if (marginMode === 'crossed') {
+    if (marginMode === 'fixed') {
         // Set margin mode to insolated
         console.log('Setting margin mode to insolated...')
         await client.futuresAccount.setMarginMode({
             ...
             changeMargin,
-            marginMode: 'fixed'
+            marginMode: 'crossed'
         })
     }
 
@@ -75,6 +76,8 @@ async function placeLongPosition() {
         const price = (await client.futuresMarket.markPrice(`${market}_UMCBL`))
             .data.markPrice
 
+        //const price = EXE_PRICE
+
         const amountBuyPAIR1 = parseFloat(amountBuyPAIR2) / parseFloat(price)
         var tp = parseFloat(parseFloat(price)
             + (parseFloat(price) * TAKE_PROFIT_PERCENT / 100)).toFixed(pricePlace)
@@ -89,9 +92,10 @@ async function placeLongPosition() {
             symbol: `${market}_UMCBL`,
             marginCoin: PAIR2,
             triggerPrice: parseFloat(price).toFixed(pricePlace),
-            side: 'open_long',
-            orderType: 'market',
-            triggerType: 'market_price',
+            executePrice:23800.5,
+            side: 'open_long', //open_short
+            orderType: 'market', //limit
+            triggerType: 'market_price', // fill_price
             size: parseFloat(amountBuyPAIR1).toFixed(sizePlace),
             presetTakeProfitPrice: tp,
             presetStopLossPrice: sl
