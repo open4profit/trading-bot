ed44fd86a400068a76280a12fb74c05c5c045507
ZIAUL HAQUE <56592240+open4profit@users.noreply.github.com>
2023-02-16 12:26:30 +0400

    Update long.js

--- futuros-bitget/long.js
+++ futuros-bitget/long.js
@@ -105,8 +105,3 @@ async function placeLongPosition() {
 }
 
 placeLongPosition()
-
-const port = 3000;
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-});
